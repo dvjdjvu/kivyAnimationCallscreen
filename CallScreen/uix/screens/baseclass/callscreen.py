ff282f9a90a7af6060cdc90195e6a7d93f2fa600
MrUDDoff <kubasov.den@bk.ru>
2022-01-25 18:35:32 +0700

    .

--- CallScreen/uix/screens/baseclass/callscreen.py
+++ CallScreen/uix/screens/baseclass/callscreen.py
@@ -1,3 +1,4 @@
+from gettext import dpgettext
 import imp
 import os
 from turtle import title
@@ -10,6 +11,11 @@ from kivymd.uix.screen import MDScreen
 from kivy.animation import Animation
 
 from kivy.properties import NumericProperty
+
+from kivy.utils import get_color_from_hex
+from kivy.core.window import Window
+
+from kivymd.color_definitions import colors
 
 #Читаем и загружаем KV файл
 with open(os.path.join(os.getcwd(), "uix", "screens", "kv", "callscreen.kv"), encoding="utf-8") as KV:
@@ -36,3 +42,69 @@ class CallScreen(MDScreen):
             Animation(blur_value=15, d=0.6, t="in_out_quad").start(self)
         else:
             Animation(blur_value=0, d=0.6, t="in_out_quad").start(self)
+
+    def animation_call_button(self, call_button):
+        if not self.open_call_box:
+            Animation(
+                x=self.center_x - call_button.width / 2,
+                y=40,
+                md_bg_color=get_color_from_hex(colors["Red"]["A700"]),
+                d=0.6,
+                t="in_out_quad",
+            ).start(call_button)
+        else:
+            Animation(
+                y=self.height * 45 / 100 + call_button.height / 2,
+                x=self.width - call_button.width - 20,
+                md_bg_color=get_color_from_hex(colors["Green"]["A700"]),
+                d=0.6,
+                t="in_out_quad",
+            ).start(call_button)
+    
+    def animation_list_box(self, list_box):
+        if not self.open_call_box:
+            Animation(
+                y=-list_box.y,
+                opacity=0,
+                d=0.6,
+                t="in_out_quad",
+            ).start(list_box)
+        else:
+            Animation(
+                y=self.height * 45 / 100 - list_box.height / 2,
+                opacity=1,
+                d=0.6,
+                t="in_out_quad",
+            ).start(list_box)
+
+    def animation_round_avatar(self, round_avatar, user_name):
+        if not self.open_call_box:
+            Animation(
+                x=self.center_x - round_avatar.width / 2,
+                y=round_avatar.y + 50,
+                d=0.6,
+                t="in_out_quad",
+            ).start(round_avatar)
+        else:
+            Animation(
+                x=self.center_x - (round_avatar.width + user_name.width + 20) / 2,
+                y=self.height * 45 / 100 + round_avatar.height,
+                d=0.6,
+                t="in_out_quad",
+            ).start(round_avatar)
+
+    def animation_user_name(self, round_avatar, user_name):
+        if not self.open_call_box:
+            Animation(
+                x=self.center_x - user_name.width / 2,
+                y=user_name.y,
+                d=0.6,
+                t="in_out_quad",
+            ).start(self.ids.user_name)
+        else:
+            Animation(
+                x=round_avatar.x,
+                y=round_avatar.center_y - user_name.height - 20,
+                d=0.6,
+                t="in_out_quad",
+            ).start(user_name)
